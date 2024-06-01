651a39fd76d97745929fb7bbb782f7b04b1ee6fb
Danish <dani.dh786@gmail.com>
2024-06-01 05:40:24 +0500

    changed cors for frontend

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ origins = [
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:8080",
-    "http://localhost:5173",
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
